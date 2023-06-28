8604ffe5c585d7e36a23261b49e80616353cfad5
Arizaki <79724270+Az-png@users.noreply.github.com>
2023-06-28 16:20:01 +0200

    v1.2

--- index.js
+++ index.js
@@ -8,17 +8,28 @@ const packageJsonPath = path.join(__dirname, 'package.json');
 const indexJsUrl = 'https://raw.githubusercontent.com/Az-png/Extaz-rpc/main/index.js';
 const indexJsPath = path.join(__dirname, 'index.js');
 
+
+console.log("\x1b[31m%s\x1b[0m", "\n\n\n/$$$$$$$$             /$$                        ");
+console.log("\x1b[31m%s\x1b[0m", "| $$_____/            | $$                        ");
+console.log("\x1b[31m%s\x1b[0m", "| $$       /$$   /$$ /$$$$$$    /$$$$$$  /$$$$$$$$");
+console.log("\x1b[31m%s\x1b[0m", "| $$$$$   |  $$ /$$/|_  $$_/   |____  $$|____ /$$/");
+console.log("\x1b[31m%s\x1b[0m", "| $$__/    \\  $$$$/   | $$      /$$$$$$$   /$$$$/ ");
+console.log("\x1b[31m%s\x1b[0m", "| $$        >$$  $$   | $$ /$$ /$$__  $$  /$$__/  ");
+console.log("\x1b[31m%s\x1b[0m", "| $$$$$$$$ /$$/\\  $$  |  $$$$/|  $$$$$$$ /$$$$$$$$");
+console.log("\x1b[31m%s\x1b[0m", "|________/|__/  \\__/   \\___/   \\_______/|________/");
+
+
 function checkForUpdates() {
-  console.log('\nVérification des mises à jour...');
+  console.log('\x1b[95m' + '\n\nVérification des mises à jour...' + '\x1b[0m');
 
   request(packageJsonUrl, (error, response, body) => {
     if (error) {
-      console.error('\nUne erreur s\'est produite lors de la vérification des mises à jour :', error);
+      console.error('\x1b[31m' + '\nUne erreur s\'est produite lors de la vérification des mises à jour :', error + '\x1b[0m');
       return;
     }
 
     if (response.statusCode !== 200) {
-      console.error('\nLa requête de vérification des mises à jour a retourné une réponse non valide :', response.statusCode);
+      console.error('\x1b[31m' +  '\nLa requête de vérification des mises à jour a retourné une réponse non valide :', response.statusCode + '\x1b[0m');
       return;
     }
 
@@ -26,51 +37,51 @@ function checkForUpdates() {
 
     fs.readFile(packageJsonPath, 'utf-8', (err, data) => {
       if (err) {
-        console.error('\nUne erreur s\'est produite lors de la lecture du fichier package.json :', err);
+        console.error('\x1b[31m' +  '\nUne erreur s\'est produite lors de la lecture du fichier package.json :', err + '\x1b[0m');
         return;
       }
 
       const currentPackageJson = JSON.parse(data);
 
       if (currentPackageJson.version !== packageJsonData.version) {
-        console.log('\nUne nouvelle version est disponible. Téléchargement en cours...');
+        console.log('\x1b[33m' + '\nUne nouvelle version est disponible. Téléchargement en cours...' + '\x1b[0m');
 
         request(indexJsUrl, (err, res, body) => {
           if (err) {
-            console.error('\nUne erreur s\'est produite lors du téléchargement de la mise à jour :', err);
+            console.error('\x1b[31m' +  '\nUne erreur s\'est produite lors du téléchargement de la mise à jour :', err  + '\x1b[0m');
             return;
           }
 
           if (res.statusCode !== 200) {
-            console.error('\nLa requête de téléchargement de la mise à jour a retourné une réponse non valide :', res.statusCode);
+            console.error('\x1b[31m' +  '\nLa requête de téléchargement de la mise à jour a retourné une réponse non valide :', res.statusCode + '\x1b[0m');
             return;
           }
 
           fs.writeFile(indexJsPath, body, 'utf-8', (error) => {
             if (error) {
-              console.error('\nUne erreur s\'est produite lors de l\'écriture du fichier index.js :', error);
+              console.error('\x1b[31m' +  '\nUne erreur s\'est produite lors de l\'écriture du fichier index.js :', error + '\x1b[0m');
               return;
             }
 
-            console.log('\nMise à jour effectuée avec succès.');
+            console.log('\x1b[32m' + '\nMise à jour effectuée avec succès.' + '\x1b[0m');
 
             // Mettre à jour le fichier package.json
             fs.writeFile(packageJsonPath, JSON.stringify(packageJsonData, null, 2), 'utf-8', (error) => {
               if (error) {
-                console.error('\nUne erreur s\'est produite lors de la mise à jour du fichier package.json :', error);
+                console.error('\x1b[31m' + '\nUne erreur s\'est produite lors de la mise à jour du fichier package.json :', error + '\x1b[0m');
                 return;
               }
 
-              console.log('\nLe fichier package.json a été mis à jour.');
+              console.log('\x1b[32m' + '\nLe fichier package.json a été mis à jour.' + '\x1b[0m');
 
               // Redémarrer le script
-              console.log('\nRedémarrage du script...');
+              console.log('\x1b[36m' + '\nRedémarrage du script...' + '\x1b[0m');
               process.exit();
             });
           });
         });
       } else {
-        console.log('\nLe script est à jour. Aucune mise à jour disponible.');
+        console.log('\x1b[34m' + '\nLe script est à jour. Aucune mise à jour disponible.' + '\x1b[0m');
       }
     });
   });
@@ -105,16 +116,16 @@ function updateDiscordStatus() {
     instance: false,
   })
   .catch((error) => {
-    console.error('Erreur lors de la mise à jour de la présence Discord :', error);
+    console.error('\x1b[31m' +  'Erreur lors de la mise à jour de la présence Discord :', error + '\x1b[0m');
   });
 }
 
 // Connectez-vous à DiscordRPC
 rpc.login({ clientId })
   .then(() => {
-    console.log('\nConnecté à Discord avec succès.');
+    console.log('\nConnecté à Discord avec succès.' + '\u2714');
     setInterval(updateDiscordStatus, 3000);
   })
   .catch((error) => {
-    console.error('Erreur lors de la connexion à Discord :', error);
+    console.error('\x1b[31m' +  'Erreur lors de la connexion à Discord :', error + '\x1b[0m');
   });
